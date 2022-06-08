da221017894faf01f41cbcad35f22f87567b6914
soni240 <s.fegade21@gmail.com>
2022-06-08 23:05:17 +0530

    [Gunavanti]add addressbook name with realations

--- UC8-sort.sql
+++
@@ -1,6 +0,0 @@
------- UC 8: Ability to retrieve entries sorted alphabetically ------
-
-select *
-from Address_Book_Table
-where City='Bangalore'
-order by(FirstName)
---
+++ UC9-add addressbook name with realations....sql
@@ -0,0 +1,28 @@
+------ UC 8: Ability to retrieve entries sorted alphabetically ------
+
+select *
+from Address_Book_Table
+where City='Bangalore'
+order by(FirstName)
+
+------ UC 9: Identify each Address Book with name andType ------
+
+alter table Address_Book_Table
+add AddressBookName varchar(100),
+Type varchar(100)
+
+--Update values for Type=Friends--
+update Address_Book_Table
+set AddressBookName='FriendName',Type='Friends'
+where FirstName='Harsha' or FirstName='Aarchna'
+
+--Update values for Type=Family--
+update Address_Book_Table
+set AddressBookName='Mom',Type='Family'
+where FirstName='Pramela'
+
+--Update values for Type=Profession--
+update Address_Book_Table
+set AddressBookName='Manager',Type='Profession'
+where FirstName='Meena'
+
